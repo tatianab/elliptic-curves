11ccab9cac5f1c5966e2bb8387dda4f10536b33c
Tatiana Bradley <tatianaebradley@gmail.com>
2014-10-16 19:06:56 -0700

    Create setup.py

---
+++ primecurves/setup.py
@@ -0,0 +1,11 @@
+# FILE: setup.py
+
+from setuptools import setup
+
+setup(name='primecurves',
+      version='0.1',
+      description='Elliptic Curve Cryptography - Curves over Prime Fields',
+      author='Tatiana Bradley',
+      license='Open Source',
+      packages=['primecurves'],
+      zip_safe=False)

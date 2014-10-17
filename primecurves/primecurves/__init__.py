8e5777cfb520e32c3f5da25994d2b307b701e6a0
Tatiana Bradley <tatianaebradley@gmail.com>
2014-10-16 19:10:05 -0700

    Create __init__.py

---
+++ primecurves/primecurves/__init__.py
@@ -0,0 +1,16 @@
+# FILE: __init__.py
+
+# OUTSIDE IMPORTS
+from random import randrange
+
+# STRUCTURES
+from primefieldelement import *
+from primecurve import *
+from primepoint import *
+
+# HELPERS
+from timer import *
+
+# ALGORITHMS
+from pollardsrho import *
+from bruteforcelog import *

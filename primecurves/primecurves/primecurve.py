ef3cee96ae65de061fbaeb156574253baec700cf
Tatiana Bradley <tatianaebradley@gmail.com>
2014-10-16 19:15:16 -0700

    Update primecurve.py

--- primecurves/primecurves/primecurve.py
+++ primecurves/primecurves/primecurve.py
@@ -2,6 +2,9 @@
 ## /author Tatiana Bradley
 ## /brief Implementation of the PrimeCurve class.
 
+from primepoint import *
+from primefieldelement import *
+
 class PrimeCurve:
 
     """
@@ -25,6 +28,8 @@ class PrimeCurve:
         self.a = a;
         self.b = b;
         self.prime = prime;
+        self.allPoints = [PrimePoint.inf()];
+        self.order = None
 
     @classmethod
     def default(cls):
@@ -34,3 +39,37 @@ class PrimeCurve:
 
     def __repr__(self):
         return "y^2 = x^3 + " + str(self.a) + "x + " + str(self.b) + " over F_" + str( self.prime )
+
+
+    # OTHER FUNCTIONS
+    def onCurve(self, x, y):
+        x = PrimeFieldElement(x, self.prime)
+        y = PrimeFieldElement(y, self.prime)
+        lhs = y ** 2
+        rhs = x ** 3 + x * self.a + self.b
+        return (lhs == rhs)
+    
+    # GET A "RANDOM" POINT (prelim)
+    def getPoint(self):
+        found = False
+        while (not found):
+            x = PrimeFieldElement.random(self.prime).value
+            for y in range(self.prime):
+                if (self.onCurve(x, y)):
+                    return PrimePoint(x, y, self)
+
+    # GET ALL THE POINTS!
+    def getAllPoints(self):
+        for x in range(self.prime):
+            for y in range(self.prime):
+                if (self.onCurve(x, y)):
+                    self.allPoints += [ PrimePoint(x, y, self) ]
+
+    def getOrder(self):
+        if self.order is None:
+            if len(self.allPoints) > 1:
+                self.order = len(self.allPoints)
+            else:
+                return "NO INFO"
+                # ^ NEEDS TO BE MODIFIED
+        return self.order

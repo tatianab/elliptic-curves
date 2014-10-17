5f85bd6244856a23701115b61d1c5db2fc239e04
Tatiana Bradley <tatianaebradley@gmail.com>
2014-10-16 20:32:42 -0700

    Update padic.py

--- primecurves/primecurves/padic.py
+++ primecurves/primecurves/padic.py
@@ -1,7 +1,18 @@
 """
- /file pAdic.py
+ /file padic.py
  /author Tatiana Bradley
  /brief A class representing a p-adic in Q_p.
+ 
 """
 
 # TODO
+
+class PAdic:
+      """ 
+      A p-adic number. 
+      
+      DATA:
+      coeffs (array) - the coefficients of the number, truncated.
+      """
+      
+      pass
